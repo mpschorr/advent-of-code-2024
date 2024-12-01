0355afaba8b850ea39a2ae2a9dc356f7dc156ef8
mpschorr <real.jeel@gmail.com>
2024-12-01 13:09:33 -0500

    utilities

---
+++ src/util.kt
@@ -0,0 +1,18 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+/**
+ * day system, set day at beginning & just use [testInput] and [input] getters
+ */
+var day = 0
+
+val paddedDay get() = day.toString().padStart(2, '0')
+fun getDayFile(fileName: String) = Path("src/$paddedDay/$fileName").readText().trim().lines()
+val testInput by lazy { getDayFile("test.txt") }
+val input by lazy { getDayFile("input.txt") }
+
+
+fun Any?.println() = println(this)
+fun Any?.println(label: String) {
+    println("$label: $this")
+}
